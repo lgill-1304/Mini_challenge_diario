8e476b6717932f1cfed5f12fbec83d43e2c18a9b
lgill-1304 <Laurita21_89@hotmail.com>
2024-06-22 11:07:02 -0400

    Add files via upload

---
+++ suma.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero1, numero2;
+    cout << "Introduce el primer numero: ";
+    cin >> numero1;
+    cout << "Introduce el segundo numero: ";
+    cin >> numero2;
+
+    int suma = numero1 + numero2;
+    cout << "La suma de los dos numeros es: " << suma << endl;
+
+    return 0;
+    
+}
